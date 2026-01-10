386ebb2c9b0691eaba505a8675d08f2db15b31bf
xxnuo <54252779+xxnuo@users.noreply.github.com>
2026-01-10 21:22:03 +0800

    Update vite.config.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,10 +5,23 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1984',
+        changeOrigin: true,
+      },
     },
   },
 })
