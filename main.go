db0f18aa5f9542f64ae9b5c2af4de5de0daf2070
xxnuo <alexcheery5@gmail.com>
2026-05-02 21:53:35 +0800

    chore: bump version v0.2.19

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func printAccessibleAddresses(host, port, scheme string) {
 }
 
 // @title VibeGo API
-// @version 0.2.18
+// @version 0.2.19
 // @description VibeGo 后端服务 API
 // @host localhost:1984
 // @BasePath /api
